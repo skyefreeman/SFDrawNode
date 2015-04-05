6070d9500406a5fe644ca6e41de5e658e215d206
Skye <gskye@aol.com>
2015-04-05 17:17:53 -0400

    Initial Commit

---
+++ SFDrawNode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SFDrawNode
+//
+//  Created by Skye on 4/5/15.
+//  Copyright (c) 2015 Skye Freeman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SFDrawNode/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  SFDrawNode
+//
+
+//  Copyright (c) 2015 Skye Freeman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
